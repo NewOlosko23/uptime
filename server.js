03c174d79f9376b5d8efff5f999d46969d9810d5
NewOlosko23 <oloogeorge633@gmail.com>
2025-09-23 19:18:56 +0300

    uptime6

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ const corsOptions = {
       process.env.CLIENT_URL || "http://localhost:3000",
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://uptime1.onrender.com/",
+      "https://uptime1.onrender.com",
       "http://localhost:3001", // Additional fallback
     ];
 
